9ebf682e09f80fea4b3aa27333bf77f71addf486
KKKHun2 <1224kim2son@naver.com>
2023-07-18 00:18:43 +0900

    ⚡api 및 enter수정

--- libs/client/useMutation.tsx
+++ libs/client/useMutation.tsx
@@ -1,3 +1,9 @@
+import { useState } from "react";
+interface UseMutationState {
+  loading: boolean;
+  data?: object;
+  error?: object;
+}
 type UseMutationResult = [(data: any) => void, UseMutationState];
 
 export default function useMutation(url: string): UseMutationResult {
